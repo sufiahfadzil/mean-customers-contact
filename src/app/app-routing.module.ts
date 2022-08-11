caaa2c6bee8e3d1c02f59f885e703465fe3e1af8
Sufiah Fadzil <sufiahfadzil@gmail.com>
2022-08-11 21:24:36 +0800

    created app routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from '@angular/router';
+import { CustomerCreateComponent } from "./customers/customer-create/customer-create.component";
+import { CustomerListComponent } from "./customers/customer-list/customer-list.component";
+
+const routes: Routes = [
+  { path: '', component: CustomerListComponent },
+  { path: 'create', component: CustomerCreateComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
